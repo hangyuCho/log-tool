1206cf3952cf997f2173fdc0ed5c9a7f26ec79ff
hangyuCho <chg3376@gmail.com>
2022-07-12 23:24:13 +0900

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,119 @@
+<svelte:head>
+	<title>tool</title>
+</svelte:head>
+<div class="container">
+	<div class="top">
+		<div class="button-group">
+			<button>Button</button>
+			<button>Button</button>
+			<button>Button</button>
+			<button>Button</button>
+			<button>Button</button>
+			<button>Button</button>
+			<button>Button</button>
+			<button>Button</button>
+			<button>Button</button>
+			<button>Button</button>
+		</div>
+		<div class="button-group">
+			<button>Button</button>
+			<button>Button</button>
+			<button>Button</button>
+			<button>Button</button>
+			<button>Button</button>
+			<button>Button</button>
+			<button>Button</button>
+			<button>Button</button>
+			<button>Button</button>
+			<button>Button</button>
+		</div>
+	</div>
+	<div class="center">
+		<div class="top">
+			<div class="row">
+				<span>search condition</span>
+				<select>
+					<option>1</option>
+					<option>2</option>
+					<option>3</option>
+					<option>4</option>
+					<option>5</option>
+					<option>6</option>
+					<option>7</option>
+				</select>
+				<button>Button</button>
+				<button>Button</button>
+				<button>Button</button>
+			</div>
+		</div>
+		<div class="bottom">
+			<div class="left">
+				<div class="row">
+					<span>xxx</span>
+					<input type="text"/>
+					<input type="time"/>
+					<span> - </span>
+					<input type="text"/>
+					<input type="time"/>
+				</div>
+				<div class="row">
+					<span>xxx</span>
+					<input type="text"/>
+					<span>xxx</span>
+					<input type="text"/>
+					<span>xxx</span>
+					<input type="text"/>
+					<span>xxx</span>
+				</div>
+				<div class="row">
+					<input type="checkbox"/>
+					<span>xxxxxxxxxx</span>
+					<button>Button</button>
+					<div>
+						<input type="checkbox"/>
+						<span>xxxxxxxxxx</span>
+						<input type="checkbox"/>
+						<span>xxxxxxxxxx</span>
+						<input type="checkbox"/>
+						<span>xxxxxxxxxx</span>
+					</div>
+					<button>Button</button>
+				</div>
+			</div>
+			<div class="right">
+				<div>
+					<input type="checkbox"/>
+					<span>xxxxxxxxxx</span>
+					<select>
+						<option>1</option>
+						<option>2</option>
+						<option>3</option>
+						<option>4</option>
+						<option>5</option>
+						<option>6</option>
+						<option>7</option>
+					</select>
+					<button>Button</button>
+					<button>Button</button>
+					<button>Button</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="bottom">
+		<div class="row">
+			<div class="left">
+				<span>xxxxxx</span>
+				<button>Button</button>
+			</div>
+			<div class="right">
+				<button>Button</button>
+				<button>Button</button>
+				<button>Button</button>
+				<button>Button</button>
+				<button>Button</button>
+				<button>Button</button>
+			</div>
+		</div>
+	</div>
+</div>
